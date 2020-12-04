327957946c18862addd07efaa7bf6ddf90baef1a
Ironipad64 <Ironipad64@gmail.com>
2020-12-03 17:52:29 -0800

    2nd commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,33 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios
+  .get(`https://dog.ceo/api/breed/pug/images/random/12`)
+  .then((res) => {
+    console.log(res);
+    // const images = res.data;
+  })
+  .catch((res) => {
+    debugger;
+  })
 
+// axios
+//   .get(`https://dog.ceo/api/breed/pug/images/random/15`)
+//   .then((res) => {
+//     const images = res.data.message;
+//     // loop over the array of images
+//     images.forEach((image) => {
+//       // create dog card
+//       const newDogCard = dogCardMaker({ imageURL: image, breed: "pug" });
+//       // console log done
+//       console.log("done");
+//       // append to entry point
+//       entryPoint.appendChild(newDogCard);
+//     });
+//   })
+//   .catch((err) => {
+//     debugger;
+//   });
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -34,21 +60,53 @@ const followersArray = [];
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
+    <div class="card"> //card
+      <img src={image url of user} /> // cardImage
+      <div class="card-info">  // cardInfo
+        <h3 class="name">{users name}</h3> // cardTitle
+        <p class="username">{users user name}</p> // cardUsename
+        <p>Location: {users location}</p> // cardLocation
         <p>Profile:
           <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
+        </p>  // cardProfileURL
+        <p>Followers: {users followers count}</p>  //cardFollowers
+        <p>Following: {users following count}</p>  //CardFollowing
+        <p>Bio: {users bio}</p>  // cardBio
       </div>
     </div>
 */
+function panelMaker({ title }) {
+
+  //create
+  const card = document.createElement("div");
+  const cardImage = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const cardTitle = document.createElement("h3");
+  const cardUsername = document.createElement("p");
+  const cardLocation = document.createElement("p");
+  const cardProfileURL = document.createElement("p");
+  const cardFollowers = document.createElement("p");
+  const cardFollowing = document.createElement("p");
+  const cardBio = document.createElement("p");
+
+  //setup
+
+  card.appendChild(cardImage);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardTitle);
+  cardInfo.appendChild(cardUsername);
+  cardInfo.appendChild(cardLocation);
+  cardInfo.appendChild(cardProfileURL);
+  cardInfo.appendChild(cardFollowers);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
+  //classname
+
+  //textContext
+
+
+}
+
 
 /*
   List of LS Instructors Github username's:
